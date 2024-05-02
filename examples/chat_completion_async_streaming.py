4ccee07639c0416919ac8a5d3ad0114307ea1e7f
Lord Basil - Automate EVERYTHING <126736516+Drlordbasil@users.noreply.github.com>
2024-05-02 09:44:45 -0400

    Update chat_completion_async_streaming.py

--- examples/chat_completion_async_streaming.py
+++ examples/chat_completion_async_streaming.py
@@ -1,3 +1,6 @@
+
+
+
 import asyncio
 
 from groq import AsyncGroq
@@ -8,20 +11,22 @@ async def main() -> None:
 
     stream = await client.chat.completions.create(
         #
-        # Required parameters
+        # Required parameters 
         #
         messages=[
             # Set an optional system message. This sets the behavior of the
             # assistant and can be used to provide specific instructions for
-            # how it should behave throughout the conversation.
+            # how it should behave throughout the conversation. This means you
+            # will be explicit with its role and personality.
             {"role": "system", "content": "you are a helpful assistant."},
-            # Set a user message for the assistant to respond to.
+            # Set a user message for the assistant to respond to. This is for tasks at hand.
             {
                 "role": "user",
                 "content": "Explain the importance of low latency LLMs",
             },
         ],
-        # The language model which will generate the completion.
+        # The language model which will generate the completion. #
+        # models: llama3-70b-8192, llama3-8b-8192	, gemma-7b-it, mixtral-8x7b-32768	UPDATED MODELS: https://console.groq.com/docs/models
         model="mixtral-8x7b-32768",
         #
         # Optional parameters
@@ -29,9 +34,10 @@ async def main() -> None:
         # Controls randomness: lowering results in less random completions.
         # As the temperature approaches zero, the model will become
         # deterministic and repetitive.
+        # 0 is right on task but boringly uncreative, 0.7 is a good coding/reasoning for most.
         temperature=0.5,
         # The maximum number of tokens to generate. Requests can use up to
-        # 2048 tokens shared between prompt and completion.
+        # 2048 tokens shared between prompt and completion. This depends heavily on the model using. Check rate limits and generative limits: https://console.groq.com/docs/models
         max_tokens=1024,
         # A stop sequence is a predefined or user-specified text string that
         # signals an AI to stop generating content, ensuring its responses
